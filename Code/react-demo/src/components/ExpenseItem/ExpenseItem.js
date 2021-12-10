4f6ffd5c54aac33f191b3f5cd3d63a70080a089f
akash.dhanwar112@gmail.com <akash.dhanwar112@gmail.com>
2021-12-10 14:49:58 +0530

    Updated

--- Code/react-demo/src/components/ExpenseItem/ExpenseItem.js
+++ Code/react-demo/src/components/ExpenseItem/ExpenseItem.js
@@ -1,8 +1,11 @@
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
-import Card from '../Card/Card';
+import Card from "../Card/Card";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Hello")
+  };
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
@@ -11,7 +14,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
